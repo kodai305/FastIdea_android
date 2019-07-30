3a3d95d5202c7036033ef5eb595cee60905a50fb
matsuei <matuse.kenta@gmail.com>
2019-07-30 22:12:34 +0900

    しりとり法とマンダラチャートの画面を作成し、「作成」ボタンを押すと遷移するように遷移

--- app/src/main/java/com/example/fastidea_android/InputTheme.kt
+++ app/src/main/java/com/example/fastidea_android/InputTheme.kt
@@ -1,7 +1,9 @@
 package com.example.fastidea_android
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_input_theme.*
 
 class InputTheme : AppCompatActivity() {
@@ -15,5 +17,17 @@ class InputTheme : AppCompatActivity() {
 
         // textにメソッド名を表示
         method_name.text = "メソッド名: " + glob.METHOD_NAME
+
+        // 作成ボタンを押下したら、それぞれの手法画面に遷移
+        val button = findViewById<Button>(R.id.create_theme_button)
+        button.setOnClickListener {
+            val intent: Intent
+            if (glob.METHOD_NAME == "Siritori") {
+                intent = Intent(this, Siritori::class.java)
+            } else {
+                intent = Intent(this, Mandara::class.java)
+            }
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/fastidea_android/Mandara.kt
@@ -0,0 +1,12 @@
+package com.example.fastidea_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Mandara : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mandara)
+    }
+}
