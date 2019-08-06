968e1a5fa8e010f15b6695dd07c5021c9aedff5a
matsuei <matuse.kenta@gmail.com>
2019-08-07 07:19:30 +0900

    SiritoriObjを追加

--- app/src/main/java/com/example/fastidea_android/TopPage.kt
+++ app/src/main/java/com/example/fastidea_android/TopPage.kt
@@ -3,7 +3,6 @@ package com.example.fastidea_android
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_top_page.*
 
